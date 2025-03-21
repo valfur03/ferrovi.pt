904fcccea39d20bd19e6c29343ab388ada0eed8a
valfur03 <valentin.furmanek@gmail.com>
2025-03-21 21:34:04 +0100

    feat: add Providers

---
+++ src/app/providers.tsx
@@ -0,0 +1,7 @@
+import { PropsWithChildren } from "react";
+
+export type ProvidersProps = PropsWithChildren;
+
+export const Providers = ({ children }: ProvidersProps) => {
+    return <>{children}</>;
+};
