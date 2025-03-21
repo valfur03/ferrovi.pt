079ce17e75967eff9e6137525d40687c3e80058d
valfur03 <valentin.furmanek@gmail.com>
2025-03-21 22:41:09 +0100

    refactor: restore typing of both key and value of metroStations object

--- src/data/metro-stations.ts
+++ src/data/metro-stations.ts
@@ -1,9 +1,11 @@
 import { MetroStation } from "@/types/metro-station";
 
-export const metroStations: Record<string, MetroStation> = {
+export const createMetroStations = <T extends Record<string, MetroStation>>(stations: T) => stations;
+
+export const metroStations = createMetroStations({
     chatelet: { name: "Châtelet", line: ["1", "4", "7", "11", "14"] },
     maubertMutualite: { name: "Maubert – Mutualité", line: ["10"] },
     porteDeClichy: { name: "Porte de Clichy", line: ["13", "14"] },
     porteDesLilas: { name: "Porte des Lilas", line: ["3bis", "11"] },
-};
+});
 export const metroStationsList = Object.values(metroStations);
