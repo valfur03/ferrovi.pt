2cda693dde1d6e2034418a5968a2f54feb7310c6
valfur03 <valentin.furmanek@gmail.com>
2025-03-22 12:00:17 +0100

    feat: add a script to generate all metro stations

---
+++ scripts/format-metro-stations-open-data.ts
@@ -0,0 +1,58 @@
+#!/usr/bin/env ts-node
+
+import { readFile } from "fs/promises";
+import path from "node:path";
+import { MetroStation } from "@/types/metro-station";
+import { writeFile } from "node:fs/promises";
+import { MetroLine } from "@/types/metro-line";
+
+/**
+ * JSON file may be downloaded from: https://data.iledefrance-mobilites.fr/explore/dataset/emplacement-des-gares-idf/information.
+ */
+const INPUT_FILE_PATH = path.join("..", "public", "metro.json");
+const OUTPUT_FILE_PATH = path.join("..", "public", "out-metro.json");
+
+export type InputStation = { mode: "METRO" | string; indice_lig: string; nom_gares: string };
+export type OutputStation = MetroStation;
+
+function slugFromName(name: string) {
+    return name
+        .normalize("NFD") // Decomposes accents
+        .replace(/[̀-ͯ]/g, "") // Removes accents
+        .replace(/[–-]/g, " ") // Replace hyphens (including long ones) with spaces
+        .replace(/('|’)/g, " ") // Replace apostrophes with spaces
+        .split(" ") // Split into words
+        .map((word, index) =>
+            index === 0 ? word.toLowerCase() : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase(),
+        ) // CamelCase transformation
+        .join("");
+}
+
+async function main() {
+    const data = await readFile(INPUT_FILE_PATH)
+        .then<Array<InputStation>>((buffer) => {
+            return JSON.parse(buffer.toString());
+        })
+        .catch((error) => {
+            console.error(error);
+            return null;
+        });
+
+    if (data === null) {
+        return process.exit(1);
+    }
+
+    const output = data.reduce<Record<string, OutputStation>>((acc, { mode, nom_gares, indice_lig }) => {
+        if (mode === "METRO") {
+            const stationName = slugFromName(nom_gares);
+            const station = acc[stationName] ?? { name: nom_gares, line: [] };
+
+            return { ...acc, [stationName]: { ...station, line: [...station.line, indice_lig as MetroLine] } };
+        }
+        return acc;
+    }, {});
+
+    return writeFile(OUTPUT_FILE_PATH, JSON.stringify(output));
+}
+
+main();
