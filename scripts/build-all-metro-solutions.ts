88123044b2b11d81f4b2d649fcdeb3cf82acde7a
valfur03 <valentin.furmanek@gmail.com>
2025-03-22 21:04:03 +0100

    feat: add script to build all metro solutions

---
+++ scripts/build-all-metro-solutions.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env ts-node
+
+import { writeFileSync } from "node:fs";
+import path from "node:path";
+import { allMetroSolutions } from "@/data/metro-stations";
+
+const OUTPUT_FILE_PATH = path.join("..", "public", "solutions.json");
+
+async function main() {
+    writeFileSync(OUTPUT_FILE_PATH, JSON.stringify(allMetroSolutions, undefined, 2));
+}
+
+main();
