181158d5f0d07ead22ca0a1e936f38d0e5662615
valfur03 <valentin.furmanek@gmail.com>
2025-03-24 12:49:40 +0100

    feat: remove cached solutions

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,16 +1,13 @@
-import { metroStations, metroStationsList } from "@/data/metro-stations";
+import { metroGraph, metroStations, metroStationsList } from "@/data/metro-stations";
 import { GuessForm } from "@/components/GuessForm/GuessForm";
 import { GameGuesses } from "@/components/GameGuesses/GameGuesses";
+import { getShortestPathFromAToB } from "@/lib/metro-graph";
 
 export const Game = async () => {
-    const allMetroSolutions = await import("@/data/computed/solutions.json");
-
     const from = Math.floor(Math.random() * metroStationsList.length);
     const to = (from + Math.floor(Math.random() * (metroStationsList.length - 1) + 1)) % metroStationsList.length;
 
-    const solution = (allMetroSolutions as unknown as Record<string, Record<string, Array<[string | null, string]>>>)[
-        metroStationsList[from].id
-    ][metroStationsList[to].id];
+    const solution = getShortestPathFromAToB(metroGraph, metroStationsList[from].id, metroStationsList[to].id);
     const path = solution.map(([, metroStation]) => {
         return metroStations[metroStation as keyof typeof metroStations];
     });
