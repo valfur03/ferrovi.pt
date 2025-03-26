226878b5ff12bb4ff0e010f9fb46c08427d1d248
valfur03 <valentin.furmanek@gmail.com>
2025-03-26 13:51:37 +0100

    feat: add stack to error

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -7,6 +7,7 @@ export default function Error({ error, reset }: { error: Error & { digest?: stri
             <p>{error.name}</p>
             <p>{error.message}</p>
             <p>{error.digest}</p>
+            <p>{error.stack}</p>
             <button onClick={() => reset()}>Try again</button>
         </div>
     );
