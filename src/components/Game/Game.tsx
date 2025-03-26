5738c993b9c6d2f848703e63e1652a5532337ffe
valfur03 <valentin.furmanek@gmail.com>
2025-03-26 17:01:26 +0100

    feat: add button to create new game

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,9 +1,9 @@
-import { metroGraph, metroStations, metroStationsList } from "@/data/metro-stations";
 import { GuessForm } from "@/components/GuessForm/GuessForm";
 import { GameGuesses } from "@/components/GameGuesses/GameGuesses";
 import { MapboxMetroStations } from "@/lib/mapbox/components/Mapbox/MapboxMetroStations";
 import { MAPBOX_PUBLIC_ACCESS_TOKEN } from "@/constants/env";
 import { buildRandomMetroStationsPath } from "@/app/game/actions";
+import { NewGameButton } from "@/components/NewGameButton/NewGameButton";
 
 export const Game = async () => {
     const path = await buildRandomMetroStationsPath();
@@ -13,11 +13,12 @@ export const Game = async () => {
             <div className="w-full max-w-screen-md">
                 <MapboxMetroStations accessToken={MAPBOX_PUBLIC_ACCESS_TOKEN} />
             </div>
-            <div className="w-full max-w-md">
+            <div className="w-full max-w-screen-md flex flex-col items-center">
                 <div className="flex flex-col sm:flex-row w-full sm:w-fit px-2 sm:px-0 gap-2">
                     <GameGuesses />
                     <GuessForm path={path} />
                 </div>
+                <NewGameButton />
             </div>
         </>
     );
