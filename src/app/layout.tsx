674f3e1086bf6c53ef76b7d1e48967c2d5807766
valfur03 <valentin.furmanek@gmail.com>
2025-04-02 11:18:33 +0200

    refactor: fix lint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,8 @@ import { Providers } from "@/app/providers";
 
 export const metadata: Metadata = {
     title: "Ferrovipathe : Le jeu",
-    description: "Testez tous les jours vos connaissances du métro parisien en reliant deux stations par le plus petit nombre d'arrêts !"
+    description:
+        "Testez tous les jours vos connaissances du métro parisien en reliant deux stations par le plus petit nombre d'arrêts !",
 };
 
 export default function RootLayout({
