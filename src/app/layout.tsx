23ce8260abe055d9495ae2fc55037125934555b4
valfur03 <valentin.furmanek@gmail.com>
2025-04-09 19:21:24 +0200

    feat: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Providers } from "@/app/providers";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
     title: "Ferrovipathe : Le jeu",
@@ -16,7 +17,10 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className="antialiased">
-                <Providers>{children}</Providers>
+                <Providers>
+                    {children}
+                    <Analytics />
+                </Providers>
             </body>
         </html>
     );
