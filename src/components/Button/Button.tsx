3028090835b74bfb5c7b19feefc4dc0c4acf9982
valfur03 <valentin.furmanek@gmail.com>
2025-04-14 19:45:36 +0200

    feat: allow merging Button className

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { Slot } from "@radix-ui/react-slot";
+import { cn } from "@/utils/cn";
 
 export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
     asChild?: boolean;
 };
 
-export const Button = ({ asChild, ...props }: ButtonProps) => {
+export const Button = ({ asChild, className, ...props }: ButtonProps) => {
     const Comp = asChild ? Slot : "button";
     return (
         <Comp
             {...props}
-            className="px-4 py-2 text-neutral-600 cursor-pointer border-4 font-bold border-neutral-600 rounded-lg"
+            className={cn(
+                "px-4 py-2 text-neutral-600 cursor-pointer border-4 font-bold border-neutral-600 rounded-lg",
+                className,
+            )}
         />
     );
 };
