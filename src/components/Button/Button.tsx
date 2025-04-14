91d746cbe94a86394e2eac4745d58c6ddb5f863e
valfur03 <valentin.furmanek@gmail.com>
2025-04-14 19:02:32 +0200

    feat: add asChild to Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Slot } from "@radix-ui/react-slot";
 
-export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+    asChild?: boolean;
+};
 
-export const Button = ({ ...props }: ButtonProps) => {
+export const Button = ({ asChild, ...props }: ButtonProps) => {
+    const Comp = asChild ? Slot : "button";
     return (
-        <button
+        <Comp
             {...props}
             className="px-4 py-2 text-neutral-600 cursor-pointer border-4 font-bold border-neutral-600 rounded-lg"
         />
