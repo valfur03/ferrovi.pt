2ebcee2ca5dcd6dd081cb4a13203f2f5ab9edc75
valfur03 <valentin.furmanek@gmail.com>
2025-04-15 15:57:21 +0200

    feat: add message when game is won

--- src/components/GeoGame/shared/components/GeoGameDashboard.tsx
+++ src/components/GeoGame/shared/components/GeoGameDashboard.tsx
@@ -2,17 +2,22 @@
 
 import { GameProblemStation } from "@/components/Game/shared/components/GameProblemStation";
 import { useGeoGame } from "@/contexts/geo-game/use-geo-game";
+import React from "react";
 
 export const GeoGameDashboard = () => {
     const { currentStation, score } = useGeoGame();
 
-    if (currentStation === null) {
-        return null;
-    }
-
     return (
         <div className="w-full p-4 max-w-xs md:px-0 grid grid-cols-[3fr_1fr] gap-3">
-            <GameProblemStation>{currentStation.name}</GameProblemStation>
+            {currentStation === null ? (
+                <div>
+                    <p>
+                        Vous avez fini les niveaux d&apos;aujourd&apos;hui, revenez demain pour de nouveaux challenges !
+                    </p>
+                </div>
+            ) : (
+                <GameProblemStation>{currentStation.name}</GameProblemStation>
+            )}
             <div className="text-center">
                 <p className="text-sm text-neutral-600">Score</p>
                 <p className="font-bold text-xl">{score}</p>
